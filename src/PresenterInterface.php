2b3f922cf6576c7dd3b9c7ead4ee3d4b74c9ec24
Natan Felles <natanfelles@gmail.com>
2021-07-01 12:46:07 -0300

    Add PHPDocs on Interfaces

--- src/PresenterInterface.php
+++ src/PresenterInterface.php
@@ -11,22 +11,101 @@ namespace Framework\Routing;
 
 /**
  * Interface PresenterInterface.
+ *
+ * The interface for data management via a Web Browser UI
+ * using the HTTP GET and POST methods.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Glossary/UI
  */
 interface PresenterInterface
 {
+	/**
+	 * Handles a GET request for /.
+	 *
+	 * Usage: Show a list of paginated items.
+	 *
+	 * @return mixed
+	 */
 	public function index();
 
+	/**
+	 * Handles a GET request for /new.
+	 *
+	 * Usage: Show a form with inputs to create a new item.
+	 * The POST action must go to the 'create' method.
+	 *
+	 * @return mixed
+	 */
 	public function new();
 
+	/**
+	 * Handles a POST request for /.
+	 *
+	 * Usage: Try to create a new item. On success, redirect to the 'show' or
+	 * 'edit' method. On fail, back to the 'new' method.
+	 *
+	 * @return mixed
+	 */
 	public function create();
 
+	/**
+	 * Handles a GET request for /$id.
+	 *
+	 * Usage: Show a specific item based on the $id.
+	 *
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function show($id);
 
+	/**
+	 * Handles a GET request for /$id/edit.
+	 *
+	 * Usage: Show a form to edit a specific item based on the $id.
+	 * The POST action must go to the 'update' method.
+	 *
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function edit($id);
 
+	/**
+	 * Handles a POST request for /$id/update.
+	 *
+	 * Usage: Try to update an item based on the $id. After the process, back
+	 * to the 'edit' method and show a message.
+	 *
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function update($id);
 
+	/**
+	 * Handles a GET request for /$id/remove.
+	 *
+	 * Usage: Show an alert message about the item to be deleted based on the
+	 * $id. The confirmation action must call a POST request to the 'delete'
+	 * method.
+	 *
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function remove($id);
 
+	/**
+	 * Handles a POST request for /$id/delete.
+	 *
+	 * Usage: Try to delete an item based on the $id. On success, go to the
+	 * 'index' method and show a success message. On fail, back to the 'remove'
+	 * method and show the error message.
+	 *
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
 	public function delete($id);
 }
