8bd91364c295f4bf0177c90244eb386f84bb0312
Natan Felles <natanfelles@gmail.com>
2021-07-03 12:24:30 -0300

    Update Route Attribute

--- src/Attributes/Route.php
+++ src/Attributes/Route.php
@@ -14,31 +14,24 @@ use Attribute;
 /**
  * Class Route.
  */
-#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Route
 {
-	protected string | null $name;
-	protected array $methods;
-	protected string $path;
-	protected array $paramsOrder;
-
 	/**
 	 * Route constructor.
 	 *
-	 * @param string|null $name The Route name
 	 * @param array|string $methods The Route HTTP Methods
 	 * @param string $path The Route path
-	 * @param array $paramsOrder The Route path parameters order
+	 * @param array $argumentsOrder The Route path arguments order
+	 * @param string|null $name The Route name
+	 * @param string|null $origin The Route origin
 	 */
 	public function __construct(
-		string | null $name,
 		array | string $methods,
 		string $path,
-		array $paramsOrder = []
+		array $argumentsOrder = [],
+		string $name = null,
+		string $origin = null,
 	) {
-		$this->name = $name;
-		$this->methods = (array) $methods;
-		$this->path = $path;
-		$this->paramsOrder = $paramsOrder;
 	}
 }
