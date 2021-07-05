3bc3d4f30291d077e7ac85745d654a8f995657d4
Natan Felles <natanfelles@gmail.com>
2021-07-05 13:18:01 -0300

    Add LanguagesTest

---
+++ tests/LanguagesTest.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is part of The Framework Routing Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Routing;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class LanguagesTest.
+ */
+final class LanguagesTest extends TestCase
+{
+	protected string $langDir = __DIR__ . '/../src/Languages/';
+
+	/**
+	 * @return array<int,string>
+	 */
+	protected function getCodes() : array
+	{
+		$codes = \array_filter((array) \glob($this->langDir . '*'), 'is_dir');
+		$length = \strlen($this->langDir);
+		$result = [];
+		foreach ($codes as $dir) {
+			if ($dir === false) {
+				continue;
+			}
+			$result[] = \substr($dir, $length);
+		}
+		return $result;
+	}
+
+	public function testKeys() : void
+	{
+		$rules = require $this->langDir . 'en/routing.php';
+		$rules = \array_keys($rules);
+		foreach ($this->getCodes() as $code) {
+			$lines = require $this->langDir . $code . '/routing.php';
+			$lines = \array_keys($lines);
+			\sort($lines);
+			self::assertSame($rules, $lines, 'Language: ' . $code);
+		}
+	}
+}
