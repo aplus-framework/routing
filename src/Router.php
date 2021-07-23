b0a1e12cbef108f910896c2a37ac53209cc5257b
Natan Felles <natanfelles@gmail.com>
2021-07-23 17:56:47 -0300

    Update default route not found html

--- src/Router.php
+++ src/Router.php
@@ -123,8 +123,9 @@ class Router
                         <!doctype html>
                         <html lang="{$lang}" dir="{$dir}">
                         <head>
-                        	<meta charset="utf-8">
-                        	<title>{$title}</title>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1">
+                            <title>{$title}</title>
                         </head>
                         <body>
                         <h1>{$title}</h1>
