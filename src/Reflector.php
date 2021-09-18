18f55d9c3068fcf5f09678010b39db70ff7b5a25
Natan Felles <natanfelles@gmail.com>
2021-09-18 20:39:43 -0300

    Add Reflector class

---
+++ src/Reflector.php
@@ -0,0 +1,111 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of Aplus Framework Routing Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Routing;
+
+use Framework\Routing\Attributes\Origin;
+use Framework\Routing\Attributes\Route;
+use ReflectionClass;
+use ReflectionException;
+
+/**
+ * Class Reflector.
+ *
+ * @package routing
+ */
+class Reflector
+{
+    /**
+     * @var ReflectionClass<object>
+     */
+    protected ReflectionClass $reflection;
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T>|object $routeActions
+     *
+     * @throws ReflectionException
+     */
+    public function __construct(object | string $routeActions)
+    {
+        $this->reflection = new ReflectionClass($routeActions);
+    }
+
+    /**
+     * @param string $method
+     *
+     * @throws ReflectionException
+     *
+     * @return Route|null
+     */
+    protected function getMethodRoute(string $method) : ?Route
+    {
+        $reflectionMethod = $this->reflection->getMethod($method);
+        $route = null;
+        foreach ($reflectionMethod->getAttributes() as $attribute) {
+            if ($attribute->getName() === Route::class) {
+                /**
+                 * @var Route $route
+                 */
+                $route = $attribute->newInstance();
+            }
+        }
+        return $route;
+    }
+
+    /**
+     * @return array<int,string>
+     */
+    protected function getObjectOrigins() : array
+    {
+        $origins = [];
+        foreach ($this->reflection->getAttributes() as $attribute) {
+            if ($attribute->getName() === Origin::class) {
+                /**
+                 * @var Origin $origin
+                 */
+                $origin = $attribute->newInstance();
+                $origins[] = $origin->getOrigin();
+            }
+        }
+        return $origins;
+    }
+
+    /**
+     * @throws ReflectionException
+     *
+     * @return array<mixed>
+     */
+    public function getRoutes() : array
+    {
+        $origins = $this->getObjectOrigins();
+        $routes = [];
+        foreach ($this->reflection->getMethods() as $method) {
+            if ( ! $method->isPublic()) {
+                continue;
+            }
+            $route = $this->getMethodRoute($method->getName());
+            if ($route === null) {
+                continue;
+            }
+            $origin = $route->getOrigin();
+            $origin = $origin === null ? $origins : [];
+            $routes[] = [
+                'origins' => $origin,
+                'methods' => $route->getMethods(),
+                'path' => $route->getPath(),
+                'arguments' => $route->getArguments(),
+                'name' => $route->getName(),
+                'action' => $method->class . '::' . $method->name,
+            ];
+        }
+        return $routes;
+    }
+}
---
+++ tests/ReflectorTest.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * This file is part of Aplus Framework Routing Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Routing;
+
+use Framework\Routing\Reflector;
+use PHPUnit\Framework\TestCase;
+use Tests\Routing\Support\UsersRouteActionsPresenter;
+use Tests\Routing\Support\UsersRouteActionsResource;
+use Tests\Routing\Support\WithoutRouteActions;
+
+/**
+ * Class ReflectorTest.
+ */
+final class ReflectorTest extends TestCase
+{
+    public function testWithoutRouteActions() : void
+    {
+        $reflector = new Reflector(new WithoutRouteActions());
+        self::assertSame([], $reflector->getRoutes());
+    }
+
+    public function testWithoutOrigins() : void
+    {
+        $reflector = new Reflector(new UsersRouteActionsPresenter());
+        self::assertContains([
+            'origins' => [],
+            'methods' => ['GET'],
+            'path' => '/users',
+            'arguments' => '*',
+            'name' => null,
+            'action' => UsersRouteActionsPresenter::class . '::index',
+        ], $reflector->getRoutes());
+        self::assertContains([
+            'origins' => [],
+            'methods' => ['POST'],
+            'path' => '/users',
+            'arguments' => '*',
+            'name' => null,
+            'action' => UsersRouteActionsPresenter::class . '::create',
+        ], $reflector->getRoutes());
+        self::assertContains([
+            'origins' => [],
+            'methods' => ['POST'],
+            'path' => '/users/{int}/delete',
+            'arguments' => '*',
+            'name' => null,
+            'action' => UsersRouteActionsPresenter::class . '::delete',
+        ], $reflector->getRoutes());
+    }
+
+    public function testWithOrigins() : void
+    {
+        $reflector = new Reflector(UsersRouteActionsResource::class);
+        self::assertContains([
+            'origins' => [
+                'http://domain.com',
+                'http://api.domain.xyz',
+            ],
+            'methods' => ['GET'],
+            'path' => '/users',
+            'arguments' => '*',
+            'name' => null,
+            'action' => UsersRouteActionsResource::class . '::index',
+        ], $reflector->getRoutes());
+        self::assertContains([
+            'origins' => [
+                'http://domain.com',
+                'http://api.domain.xyz',
+            ],
+            'methods' => ['DELETE'],
+            'path' => '/users/{int}',
+            'arguments' => '0',
+            'name' => 'users.delete',
+            'action' => UsersRouteActionsResource::class . '::delete',
+        ], $reflector->getRoutes());
+    }
+}
