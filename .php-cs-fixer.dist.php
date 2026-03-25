145b648201cc97e740def8568e59e73a28204840
Natan Felles <natanfelles@gmail.com>
2026-03-25 16:12:47 -0300

    Remove parentheses

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,7 +10,7 @@
 use Framework\CodingStandard\Config;
 use Framework\CodingStandard\Finder;
 
-return (new Config())->setDefaultHeaderComment(
+return new Config()->setDefaultHeaderComment(
     'Aplus Framework Routing Library',
     'Natan Felles <natanfelles@gmail.com>'
 )->setFinder(
