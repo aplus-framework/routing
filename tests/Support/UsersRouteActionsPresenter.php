dffe34e5bad1ac82177bd57ed67dc0f54033b53e
Natan Felles <natanfelles@gmail.com>
2026-03-25 16:25:15 -0300

    Ignore phpstan errors

--- tests/Support/UsersRouteActionsPresenter.php
+++ tests/Support/UsersRouteActionsPresenter.php
@@ -29,7 +29,7 @@ class UsersRouteActionsPresenter extends RouteActions implements PresenterInterf
     }
 
     #[Route('POST', '/users')]
-    #[Route('PATCH', '/users', name: 'repeated')]
+    #[Route('PATCH', '/users', name: 'repeated')] // @phpstan-ignore-line
     public function create() : string
     {
         return __METHOD__;
--- tests/Support/UsersRouteActionsResource.php
+++ tests/Support/UsersRouteActionsResource.php
@@ -16,7 +16,7 @@ use Framework\Routing\ResourceInterface;
 use Framework\Routing\RouteActions;
 
 #[Origin('http://domain.com')]
-#[Origin('http://api.domain.xyz')]
+#[Origin('http://api.domain.xyz')] // @phpstan-ignore-line
 class UsersRouteActionsResource extends RouteActions implements ResourceInterface
 {
     #[Route('GET', '/users', origins: 'http://foo.com')]
